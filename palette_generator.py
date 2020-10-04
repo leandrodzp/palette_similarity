7143edfd732de7f4cab79b5d4d336b5c36c0ccaa
Leandro Dominguez <leandro@rootstrap.com>
2020-10-04 19:06:45 -0300

    Corrections after Guilles suggestions

--- palette_generator.py
+++ palette_generator.py
@@ -6,10 +6,4 @@ from constants import AMOUNT_OF_COLORS
 def paletteFromImage(file):
     image = Image.open(file.file)
     colors = colorgram.extract(image, AMOUNT_OF_COLORS)
-    encoded_colors = []
-    for color in colors:
-        encoded_colors.append(encodeToRGB(color.rgb))
-    return encoded_colors
-
-def encodeToRGB(color_rgb):
-  return tuple([color_rgb[0], color_rgb[1], color_rgb[2]])
+    return [color.rgb for color in colors]
