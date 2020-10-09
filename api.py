68ad33f8bc208e8e5206b69fe1289ef2d6ec03c7
Leandro Dominguez <leandro@rootstrap.com>
2020-10-09 17:21:12 -0300

    Changes palette response connects endpoint with ML model

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ import uvicorn
 
 from palette_generator import paletteFromImage
 from constants import AMOUNT_OF_COLORS
+from palette_embedding.palette_embedding import PaletteEmbeddingModel
 
 app = FastAPI()
 
@@ -12,6 +13,20 @@ async def generate_palette(file: UploadFile = File(...)):
         "number_of_colors" : AMOUNT_OF_COLORS,
         "palette" : paletteFromImage(file)
     }
+    return response
+
+@app.post("/recommendations/")
+async def generate_palette(query_params: RecommendationsRequest):
+    
+    algorithm_entry = '-'.join(query_params.palette)
+    model = PaletteEmbeddingModel()
+    embedding = model.Embed(palette=algorithm_entry)
+
+    # Here we should call the elastic searcher with query_params.filters
+    response = {
+        "recommendations": "This should be the response from Elastic",
+        }
+
     return response
 
 if __name__ == "__main__":
--- palette_generator.py
+++ palette_generator.py
@@ -6,4 +6,8 @@ from constants import AMOUNT_OF_COLORS
 def paletteFromImage(file):
     image = Image.open(file.file)
     colors = colorgram.extract(image, AMOUNT_OF_COLORS)
+<<<<<<< HEAD
     return [color.rgb for color in colors]
+=======
+    return ['%02x%02x%02x' % color.rgb  for color in colors]
+>>>>>>> 4df0a52... Changes palette response connects endpoint with ML model
