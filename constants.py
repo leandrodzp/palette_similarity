7eb0f5d97ade35b7469b71c5ced154a056d4bb2d
agusit7 <amautone@tryolabs.com>
2020-10-11 11:19:15 -0300

    Apply back and add initial frontend

--- constants.py
+++ constants.py
@@ -1,6 +1,7 @@
 AMOUNT_OF_COLORS = 5
-ELASTIC_URL = 'http://localhost:9200'
-INDEX_NAME = 'paintings'
+ELASTIC_URL = "http://localhost:9200"
+INDEX_NAME = "paintings"
 EMBEDDING_DIMS = 15
-NUMBER_OF_SHARDS = 1 # we should see this
+NUMBER_OF_SHARDS = 1  # we should see this
 NUMBER_OF_REPLICAS = 1
+URL_PATTERN = r"((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*"
