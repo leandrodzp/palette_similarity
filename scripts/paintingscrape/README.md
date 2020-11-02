b6a1ee44792dc1a131cb1e97edecdc94a1801116
Noe Bassagoda <noebassagoda@gmail.com>
2020-11-01 23:32:28 -0300

    Paintings scraper

---
+++ scripts/paintingscrape/README.md
@@ -0,0 +1,7 @@
+# NAME OF THE PROJECT
+
+This app allows you to upload a picture of your room, and based on the dominant colours palett, suggests you beaufiful matching paintings.
+
+## Architecture of the APP
+
+## Steps to install
---
+++ scripts/paintingscrape/paintingscrape/spiders/paintings_spider.py
@@ -0,0 +1,31 @@
+import scrapy
+import re
+
+URL='https://www.saatchiart.com'
+
+
+class PaintingsSpider(scrapy.Spider):
+    name = "paintings"
+
+    start_urls = [
+        f'{URL}/paintings'
+    ]
+
+    def parse(self, response):
+        for painting in response.css('div.cygeUL'):
+            product_path = painting.css('div.jDvrtz a::attr(href)')[0].get()
+            try:
+              painting.css('p.kcNkMy::text')[0].get()
+              yield {
+                  'title': painting.css('p.kcNkMy a::text')[0].get(),
+                  'artist': re.match(r"<a\b[^>]*>(.*?)<\/a>", painting.css('p.kcNkMy a')[1].get()).group(1).replace("<!-- -->", ""),
+                  'price': painting.css('p.kcNkMy::text')[0].get().strip("$").replace(',', ''),
+                  'image-url': painting.css('div.jDvrtz a img::attr(src)')[0].get(),
+                  'purchase-url': f'{URL}/{product_path}',
+              }
+            except IndexError:
+              print('sorry')
+        next_page = int(response.css("a.cMuFQi::text").get()) + 1
+        next_url = f'{URL}/paintings?page={next_page}'
+        if next_page < 200:
+          yield scrapy.Request(next_url, callback=self.parse)
