e45e72522bc5a6a3183d92a8cc5cb7f547040a6c
guilleripa <guilleripa21@gmail.com>
2020-11-11 16:07:54 -0300

    Cycle through colors to get 5.

--- api.py
+++ api.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI, File, UploadFile
 import uvicorn
+from fastapi import FastAPI, File, UploadFile
 
-from palette_generator import paletteFromImage
 from constants import AMOUNT_OF_COLORS
 from palette_embedding.palette_embedding import PaletteEmbeddingModel
+from palette_generator import paletteFromImage
 
 # Defines request objects to clean the API
 from request_objects.generate_palette_request import GeneratePaletteRequest
@@ -11,26 +11,29 @@ from request_objects.recommendations_request import RecommendationsRequest
 
 app = FastAPI()
 
+
 @app.post("/generate_palette/")
 async def generate_palette(file: UploadFile = File(...)):
     response = {
-        "number_of_colors" : AMOUNT_OF_COLORS,
-        "palette" : paletteFromImage(file)
+        "number_of_colors": AMOUNT_OF_COLORS,
+        "palette": paletteFromImage(file.file),
     }
     return response
 
+
 @app.post("/recommendations/")
 async def generate_palette(query_params: RecommendationsRequest):
-    algorithm_entry = '-'.join(query_params.palette)
+    algorithm_entry = "-".join(query_params.palette)
     model = PaletteEmbeddingModel()
     embedding = model.Embed(palette=algorithm_entry)
 
     # Here we should call the elastic searcher with query_params.filters
     response = {
         "recommendations": "This should be the response from Elastic",
-        }
+    }
 
     return response
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
--- palette_generator.py
+++ palette_generator.py
@@ -1,9 +1,18 @@
+from itertools import cycle
+
 import colorgram
 from PIL import Image
 
 from constants import AMOUNT_OF_COLORS
 
+
 def paletteFromImage(file):
-    image = Image.open(file.file)
+    image = Image.open(file)
     colors = colorgram.extract(image, AMOUNT_OF_COLORS)
-    return ['%02x%02x%02x' % color.rgb  for color in colors]
+    colors_loop = cycle(colors)
+    rgbs = []
+    for color in colors_loop:
+        rgbs.append("%02x%02x%02x" % color.rgb)
+        if len(rgbs) == 5:
+            break
+    return rgbs
